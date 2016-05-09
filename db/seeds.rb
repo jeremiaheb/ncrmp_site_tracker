3ef47897f88308ff203e7324722bcc66b941b223
jeremiaheb <jblondeau@gmail.com>
2016-05-09 11:06:32 -0400

    added point for FKeys and DRTO

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,37 +3,37 @@
 #
 
 
-#Location.delete_all("region_id = 1")
-#open("db/SeedData/fk14_samplist.csv") do |samples|
-  #samples.read.each_line do |sample|
-    #id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
-    #Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
-  #end
-#end
-
-#Location.delete_all("region_id = 2")
-#open("db/SeedData/drto.csv") do |samples|
-  #samples.read.each_line do |sample|
-    #id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
-    #Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
-  #end
-#end
-
-Location.delete_all("region_id = 3")
-#open("db/SeedData/SEFCRI_2015.csv") do |samples|
-  #samples.read.each_line do |sample|
-    #id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
-    #Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
-  #end
-#end
-
-Location.delete_all("region_id = 4")
-open("db/SeedData/PR_Sites_2016.csv") do |samples|
+Location.delete_all("region_id = 1")
+open("db/SeedData/Fl_Keys_2016_Points.csv") do |samples|
   samples.read.each_line do |sample|
     id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
     Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
   end
 end
+
+Location.delete_all("region_id = 2")
+open("db/SeedData/DRTO_2016_Points.csv") do |samples|
+  samples.read.each_line do |sample|
+    id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
+    Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
+  end
+end
+
+#Location.delete_all("region_id = 3")
+ #open("db/SeedData/SEFCRI_2015.csv") do |samples|
+  #samples.read.each_line do |sample|
+    #id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
+    #Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
+  #end
+#end
+
+#Location.delete_all("region_id = 4")
+#open("db/SeedData/PR_Sites_2016.csv") do |samples|
+  #samples.read.each_line do |sample|
+    #id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
+    #Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
+  #end
+#end
 
 #Location.delete_all("region_id = 5")
 #open("db/SeedData/STX_2015.csv") do |samples|
